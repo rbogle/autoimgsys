8ad9c6ea5149944c0fb578292d119e6b18077ed9
rbogle <rbogle@usgs.gov>
2014-09-03 15:23:51 -0700

    init ui with flask, flask-admin, flask-login,flask-sqla

--- ais/test/test_task.py
+++ ais/test/test_task.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
+#
+#    This software is in the public domain because it contains materials
+#    that originally came from the United States Geological Survey, 
+#    an agency of the United States Department of Interior.
+#    For more information, see the official USGS copyright policy at
+#    http://www.usgs.gov/visual-id/credit_usgs.html#copyright
+#
+#   <author> Rian Bogle </author>
+
 from ais.task import Task
 import logging
 
 class Test_Task(Task):
     
     def run(self, **kwargs):     
-        logging.info(self._print_keyword_args("Run called", **kwargs))        
+        logging.info(self._print_keyword_args("Test_Task Run called", **kwargs))        
         
     def respond(self, event):
-        logging.info("Respond called with event: %s" % event) 
+        logging.info("Test_Task Respond called with event: %s" % event) 
         
     def __init__(self, **kwargs):
         Task.__init__(self,**kwargs)
