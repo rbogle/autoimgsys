6e62599bbd35cf4f5185613c124dcfe72e3dff08
rbogle <rbogle@usgs.gov>
2014-10-27 11:50:45 -0700

    updates to phenocam plugin

--- ais/ui/__init__.py
+++ ais/ui/__init__.py
@@ -7,6 +7,9 @@ import config
 flask = Flask('ais')
 flask.template_folder = flask.root_path
 flask.static_folder = flask.root_path+"/ui/static"
+
+config.DATABASE_PATH = flask.root_path+config.DATABASE_PATH
+config.SQLALCHEMY_DATABASE_URI=config.DATABASE_PREFIX+config.DATABASE_PATH+config.DATABASE_FILE
 flask.config.from_object(config)
  
 db = SQLAlchemy(flask)
--- ais/ui/config.py
+++ ais/ui/config.py
@@ -3,9 +3,10 @@ import os
 DEBUG=True
 # Create in-memory database
 DATABASE_FILE='ais_db.sqlite'
-DATABASE_PATH = os.getcwd()+'/ais/db/'
+DATABASE_PATH = '/db/'
 DATABASE_PREFIX = 'sqlite:///'
-SQLALCHEMY_DATABASE_URI=DATABASE_PREFIX+DATABASE_PATH+DATABASE_FILE
+SQLALCHEMY_DATABASE_URI=""
+
 SQLALCHEMY_ECHO = False
 
 SECRET_KEY = os.urandom(24)
