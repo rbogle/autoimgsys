85d096ca75378d87ab481468ea723ec6eb02fe82
rbogle <rbogle@usgs.gov>
2016-03-30 21:26:59 -0700

    layout tweaks

--- ais/plugins/system/system.py
+++ ais/plugins/system/system.py
@@ -71,9 +71,14 @@ class System(Task):
         if modal is not None:
             return self.get_widget_modal(modal)
         #return default page    
+        p = {
+            'sys': self.path+"/sys_panel.html", 
+            'dsk': self.path+"/dsk_panel.html",
+            'net': self.path+"/net_panel.html"
+        }
         w = self.get_widgets()
         i = self._get_sys_info()
-        return self.render(self.view_template, info=i, widgets=w)
+        return self.render(self.view_template, info=i, widgets=w, panels=p)
     
     def get_widget_modal(self, name):
         if name == 'reboot':
