690d631b2802cd92968cd9552a186146a81093ef
rbogle <rbogle@usgs.gov>
2016-04-12 11:54:19 -0700

    phidget command line helper

--- ais/plugins/phidget/phidget.py
+++ ais/plugins/phidget/phidget.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from ais.lib.relay import Relay
+import argparse,sys
 from Phidgets.PhidgetException import PhidgetException
 from Phidgets.Devices.InterfaceKit import InterfaceKit
 
@@ -63,3 +64,31 @@ class Phidget(Relay):
         except PhidgetException as e:
             self.logger.error("Phidget Exception %i: %s" % (e.code, e.details))
             raise e
+            
+if __name__ == "__main__":
+       
+    #logging.basicConfig(level=logging.DEBUG)
+    phidget = Phidget()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port", type=int, help="The Phidget port to use")
+    parser.add_argument("-s", "--set", choices=['on', 'off'], help="The value to set the phidget port: on/off")
+    args = parser.parse_args()
+    if args.set:
+        print "setting port: %d to %s" %(args.port,args.set)
+        if args.set == 'on':
+            args.set = 1
+        else:
+            args.set = 0
+        v = phidget.set_port(args.port, args.set)
+    else:
+        print "getting port: %d" %(args.port)
+        v = phidget.get_port(args.port)
+    if v:
+        v = "on"
+    else:
+        v = "off"
+        
+    print "Port %d is set to %s" %(args.port, v)
+
+        
+    
